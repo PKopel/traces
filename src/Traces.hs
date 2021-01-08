ff42cf88ebecdaad7eb9c37c41e567b292f0bcfd
PKopel <pawel.kopel2@gmail.com>
2021-01-09 00:36:45 +0100

    fix error in code computing FNF, add code computing trace

--- src/Traces.hs
+++ src/Traces.hs
@@ -1,8 +1,20 @@
 module Traces where
 
-import Data.Map as Map ( empty, insert, lookup, null, Map )
-import Data.List as List ( foldl )
-import Control.Monad.Reader ( foldM, asks, Reader )
+import           Data.Map                      as Map
+                                                ( empty
+                                                , insert
+                                                , lookup
+                                                , null
+                                                , Map
+                                                )
+import           Data.List                     as List
+                                                ( foldl
+                                                , permutations
+                                                )
+import           Control.Monad.Reader           ( foldM
+                                                , asks
+                                                , Reader
+                                                )
 
 data Item = Marker | Letter Char
 
@@ -14,37 +26,58 @@ type Set = [(Char, Char)]
 type I = Set
 type D = Set
 type FNF = [String]
+type Trace = [String]
+type Stacks = Map Char [Item]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (x ==)
 
 computeD :: Alphabet -> I -> D
-computeD alph ind = [(x,y) | x <- alph, y <- alph, (x,y) `notElem` ind]
+computeD alph ind = [ (x, y) | x <- alph, y <- alph, (x, y) `notElem` ind ]
+
+computeTrace :: String -> REnv Trace
+computeTrace w = do
+  ind <- asks independent
+  return $ filter (eqvI ind w) $ List.permutations w
+
+
+eqvI :: I -> String -> String -> Bool
+eqvI ind (a : t) (b : s) =
+  let areIndependent = a == b || (a, b) `elem` ind
+      areInOrder = count a (a : t) == count a s || count b (b : s) == count b t
+  in  (areIndependent || areInOrder) && eqvI ind t s
+eqvI _ [] [] = True
+eqvI _ _  _  = False
 
 wordToFNF :: String -> REnv FNF
 wordToFNF word = createStacks word >>= stacksToFNF
 
-createStacks :: String -> REnv (Map.Map Char [Item])
+createStacks :: String -> REnv Stacks
 createStacks = foldM placeMarkers Map.empty
 
-placeMarkers :: Map.Map Char [Item] -> Char -> REnv (Map.Map Char [Item])
-placeMarkers map char = do
+placeMarkers :: Stacks -> Char -> REnv Stacks
+placeMarkers stacks char = do
   alph <- asks alph
-  ind  <- asks independent
+  dep  <- asks dependent
   return $ List.foldl
-    (\newMap c ->
-      let item = if c == char || (char, c) `elem` ind then Letter c else Marker
-      in  placeMarker c item newMap
+    (\newStacks c -> if (c, char) `elem` dep
+      then
+        let item = if c == char then Letter c else Marker
+        in  placeMarker c item newStacks
+      else newStacks
     )
-    map
+    stacks
     alph
 
-placeMarker :: Char -> Item -> Map.Map Char [Item] -> Map.Map Char [Item]
+placeMarker :: Char -> Item -> Stacks -> Stacks
 placeMarker c marker map = case Map.lookup c map of
   Nothing       -> Map.insert c [marker] map
   Just oldStack -> Map.insert c (marker : oldStack) map
 
-stacksToFNF :: Map.Map Char [Item] -> REnv FNF
+stacksToFNF :: Stacks -> REnv FNF
 stacksToFNF stacks = stacksToFNFAcc stacks []
 
-stacksToFNFAcc :: Map.Map Char [Item] -> FNF -> REnv FNF
+stacksToFNFAcc :: Stacks -> FNF -> REnv FNF
 stacksToFNFAcc stacks acc
   | Map.null stacks = return acc
   | otherwise = do
@@ -53,14 +86,13 @@ stacksToFNFAcc stacks acc
     (newStacks, newAcc) <- foldM findStep (stacks, acc) rAlph
     stacksToFNFAcc newStacks ([] : newAcc)
 
-findStep
-  :: (Map.Map Char [Item], FNF) -> Char -> REnv (Map.Map Char [Item], FNF)
+findStep :: (Stacks, FNF) -> Char -> REnv (Stacks, FNF)
 findStep (stacks, acc@(step : rest)) char = case Map.lookup char stacks of
   Just ((Letter l) : lStack) -> rmMarkers stacks l >>= \newStacks ->
     return (Map.insert l lStack newStacks, (l : step) : rest)
   _ -> return (stacks, acc)
 
-rmMarkers :: Map.Map Char [Item] -> Char -> REnv (Map.Map Char [Item])
+rmMarkers :: Stacks -> Char -> REnv Stacks
 rmMarkers stacks char = do
   rAlph <- asks (reverse . alph)
   dep   <- asks dependent
